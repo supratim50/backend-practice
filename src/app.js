2984756de6f48faf9786e37ccd775b896d1c1efa
Supratim Mondal <supratimmondal50@gmail.com>
2024-01-13 10:59:30 +0530

    add cookieParser to app.js

--- src/app.js
+++ src/app.js
@@ -13,5 +13,6 @@ app.use(express.json({limit: "16kb"})); // accepting JSON
 
 app.use(express.urlencoded({extended: true, limit: "16kb"})); // this is for encoding URL data
 app.use(express.static("public "));
+app.use(cookieParser())
 
 export {app}
